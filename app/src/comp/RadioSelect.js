591c43e2ebcc92106ea1a5a7a017a18a70afd872
stevekutz <stkutz@gmail.com>
2020-03-02 14:27:30 -0600

    addedRadioSelect comp

---
+++ app/src/comp/RadioSelect.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+
+const RadioSelect = vals => {
+    const[toggleState, setToggleState] = React.useState(false);
+
+    console.log('vals is ', vals)
+
+    return (
+
+        <div>
+            <input 
+            type="radio" 
+            id="genericRadio" name="other Radio" 
+            checked = {toggleState}
+            onClick = { () => setToggleState(!toggleState) }
+            />
+            <label htmlFor="genericRadio">Generic Radio</label>        
+        </div>
+    )
+
+}
+
+export default RadioSelect;
+
+
+
