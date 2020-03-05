8d591812604ab0a4ce05196eeed89ceb5ccdcc17
stevekutz <stkutz@gmail.com>
2020-03-04 21:48:25 -0600

    added traditional radio group

--- app/src/App.js
+++ app/src/App.js
@@ -8,8 +8,25 @@ import RadioSelect from './comp/RadioSelect';
 function App() {
   return (
     <div className="App">
-        <RadioSelect vals = "Radio 1" typeControl = "radio"/>
-        <RadioSelect vals = "Radio 2" typeControl = "radio"/>
+        <div>
+            <RadioSelect vals = "Radio 1" typeControl = "radio"/>
+            <RadioSelect vals = "Radio 2" typeControl = "radio"/>
+        </div>
+        <div>
+            <input type="radio" id="contactChoice1"
+            name="contact" value="email"/>
+            <label for="contactChoice1">Email</label>
+        
+            <input type="radio" id="contactChoice2"
+                name="contact" value="phone"/>
+            <label for="contactChoice2">Phone</label>
+        
+            <input type="radio" id="contactChoice3"
+                name="contact" value="mail"/>
+            <label for="contactChoice3">Mail</label>
+        
+        </div>
+
         <RadioSelect vals = "Checkbox 1" typeControl = "checkbox"/>
         <RadioSelect vals = "Checkbox 2" typeControl = "checkbox"/>
         <RadioSelect vals = "Input 1" typeControl = "input"/>
